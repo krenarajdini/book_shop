537bcb4b19803f33e51343b7850b978e9fce079a
krenari <nari_ari56@hotmail.com>
2022-04-16 16:54:09 +0300

    pass validation

---
+++ js/edit.js
@@ -0,0 +1,67 @@
+let passElement = document.getElementById("password");
+let confirmPassElement = document.getElementById("cpassword");
+let passErrorMessageElement = document.getElementById("pass-error-message");
+let confirmPasswordErrorElement = document.getElementById(
+  "cpass-error-message"
+);
+
+let errors = [];
+
+passElement.onkeyup = function () {
+  pass = passElement.value;
+  confirmPass = confirmPassElement.value;
+
+  if (
+    pass == confirmPass &&
+    new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/).test(pass)
+  ) {
+    passElement.classList.remove("error-element");
+    confirmPassElement.classList.remove("error-element");
+    passErrorMessageElement.innerText = "";
+    passErrorMessageElement.classList.remove("error-message");
+  } else if (
+    pass != confirmPass ||
+    !new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/).test(pass)
+  ) {
+    passElement.classList.add("error-element");
+    confirmPassElement.classList.add("error-element");
+    passErrorMessageElement.innerHTML =
+      "<p>Password must have:</p> <p>6 to 20 characters</p> <p>contain at least one numeric digit</p>" +
+      "<p>one uppercase and one lowercase letter</p>";
+    passErrorMessageElement.classList.add("error-message");
+  }
+};
+
+confirmPassElement.onkeyup = function () {
+  pass = passElement.value;
+  confirmPass = confirmPassElement.value;
+  console.log(pass, confirmPass);
+  console.log(pass == confirmPass);
+  console.log(
+    new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/).test(confirmPass)
+  );
+  console.log(
+    pass == confirmPass &&
+      new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/).test(confirmPass)
+  );
+  if (
+    pass == confirmPass &&
+    new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/).test(confirmPass)
+  ) {
+    confirmPassElement.classList.remove("error-element");
+    passElement.classList.remove("error-element");
+    passErrorMessageElement.innerText = "";
+    passErrorMessageElement.classList.remove("error-message");
+  } else if (
+    pass != confirmPass ||
+    !new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/).test(confirmPass)
+  ) {
+    confirmPassElement.classList.add("error-element");
+    passElement.classList.add("error-element");
+    passErrorMessageElement.innerHTML =
+      "<p>Password must have:</p> <p>6 to 20 characters</p> <p>contain at least one numeric digit</p>" +
+      "<p>one uppercase and one lowercase letter</p>";
+
+    passErrorMessageElement.classList.add("error-message");
+  }
+};
